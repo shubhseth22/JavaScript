6add42073d965653ae03aaa8d3a6060dcdefded2
Shubh Seth <shubh2505seth@gmail.com>
2026-06-16 19:37:40 +0000

    container config done

---
+++ 05_iterations/one.js
@@ -0,0 +1,23 @@
+//for loop
+
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+//break and continue
+for(let i=0;i<10;i++){
+    if(i==5){
+        console.log("5 found");
+        break;
+    }
+    console.log(i);
+}
+
+
+for(let i=0;i<10;i++){
+    if(i==5){
+        console.log("5 found");
+        continue;
+    }
+    console.log(i);
+}
